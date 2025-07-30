4171e219f5a2e55cf340fffe2914ea89a6ba3912
Van <152158486+ivanajeo@users.noreply.github.com>
2025-07-30 13:41:19 +0700

    Add middleware and session token cookie handlers

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -5,6 +5,8 @@ import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/enco
 import { sha256 } from "@oslojs/crypto/sha2";
 
 import type { User, Session } from "@prisma/client";
+import { cookies } from "next/headers";
+import { cache } from "react";
 
 export function generateSessionToken(): string {
 	const bytes = new Uint8Array(20);
@@ -65,3 +67,39 @@ export async function invalidateSession(sessionId: string): Promise<void> {
 export type SessionValidationResult =
 	| { session: Session; user: User }
 	| { session: null; user: null };
+
+
+
+
+// Cookies
+export async function setSessionTokenCookie(token: string, expiresAt: Date): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", token, {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		expires: expiresAt,
+		path: "/"
+	});
+}
+
+export async function deleteSessionTokenCookie(): Promise<void> {
+	const cookieStore = await cookies();
+	cookieStore.set("session", "", {
+		httpOnly: true,
+		sameSite: "lax",
+		secure: process.env.NODE_ENV === "production",
+		maxAge: 0,
+		path: "/"
+	});
+}
+
+export const getCurrentSession = cache(async (): Promise<SessionValidationResult> => {
+	const cookieStore = await cookies();
+	const token = cookieStore.get("session")?.value ?? null;
+	if (token === null) {
+		return { session: null, user: null };
+	}
+	const result = await validateSessionToken(token);
+	return result;
+});
